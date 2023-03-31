8e0038030c1dc3d1eda1c22a7cff1c38f7ff27c5
aastha <aasthabarve0@gmail.com>
2023-03-31 23:51:09 +0530

    Welcome to Line Comparison Computation Program

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Line\n" +
-                "Comparison Computation\n" +
-                "Program");
+        System.out.println("Welcome to Line" + "Comparison Computation" + "Program");
     }
 }
